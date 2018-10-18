2697d8732ab1dd2de6385ae36a050cca6dacf2cb
Emma Lejal Glaude <emma.lejal-glaude@epfl.ch>
2018-10-18 19:45:54 +0200

    Fastext work + creating vectors for words + Webpage connection to NN

---
+++ webpage/static/js/lyrix.js
@@ -0,0 +1,23 @@
+function queryNN(){
+  /*var input = document.getElementById("input").value;*/
+  /*console.log($('form').serialize());*/
+	$.ajax({
+		url: '/predict',
+    cache : false,
+		data: $('form').serialize(),
+		type: 'POST',
+		success: function(response){
+			/*console.log("success " +response);*/
+      update_data(response);
+		},
+		error: function(error){
+      console.log(error);
+		}
+	});
+
+}
+
+function update_data(pred){
+  /*console.log("Exited queryNN()");*/
+  document.getElementById("prediction").innerHTML = "We predicted the score : "+ pred;
+}
